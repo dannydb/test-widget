6e458283b882f77e08fa630091fe989673f01bd9
Danny DeBelius <danny.debelius@gmail.com>
2017-02-20 18:22:15 -0500

    basic form behavior

---
+++ script.js
@@ -0,0 +1,36 @@
+// DOM refs
+var $bar = $('.js-bar');
+var $remaining = $('.js-remaining');
+var $amount = $('.js-amount');
+var $submit = $('.js-submit');
+var $success = $('.js-success');
+var $form = $('.js-form');
+var $help = $('.js-help');
+
+var GOAL_AMOUNT = 618;
+var CURRENT_AMOUNT = 451;
+var CURRENT_REMAINING = 167;
+
+var init = function() {
+  // Bind events
+
+  $submit.on('click', onSubmitClick);
+}
+
+var onSubmitClick = function(e) {
+  e.preventDefault();
+
+  var newAmount = $amount.val() + CURRENT_AMOUNT;
+  var barWidth = Math.floor(newAmount/GOAL_AMOUNT);
+  var newRemaining = '$' + (CURRENT_REMAINING - $amount.val());
+
+  $bar.width(barWidth + '%');
+  $remaining.text(newRemaining);
+
+  $form.css('opacity', 0);
+  $help.css('opacity', 0);
+  $success.css('opacity', 1);
+  $submit.attr('disabled',true);
+}
+
+init();
